d4d3028dd6533376411a08dd151b91051d5dc35c
Samantha Berg <sb11158@byu.edu>
2021-03-24 23:26:39 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using ASSIGNMENT9._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT9._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieListContext context { get; set; }
+
+        //constructor
+        public HomeController(ILogger<HomeController> logger, MovieListContext con)
+        {
+            _logger = logger;
+            context = con;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Movies);
+        }
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(Movie M)
+        {
+            //add data 
+            if (ModelState.IsValid)
+            {
+                context.Movies.Add(M);
+                context.SaveChanges();
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT9._0.Models
+{
+    public class MovieListContext : DbContext
+    {
+        public MovieListContext (DbContextOptions<MovieListContext> options) : base (options)
+        {}
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
